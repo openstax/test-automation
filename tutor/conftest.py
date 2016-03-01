188a175c0a56207fb08cd122fcd12f9ba7eb3f4d
Greg Fitch <greg.fitch@rice.edu>
2016-03-01 10:37:18 -0600

    PEP8 changes to Jenkins test files

--- tutor/conftest.py
+++ tutor/conftest.py
@@ -1,4 +1,9 @@
-import pytest
+"""Add command option for parameterization."""
+
+
+import pytest  # NOQA
+
 
 def pytest_addoption(parser):
+    """Add branch parameter."""
     parser.addoption('--testall', action='store_true', help='Run all tests')
--- tutor/test_assert.py
+++ tutor/test_assert.py
@@ -1,16 +1,26 @@
+"""Jenkins-CI test file."""
+
+
 import pytest
 
+
 slow = pytest.mark.skipif(
-	not pytest.config.getoption('--testall'),
-	reason='Ignoring slow or incomplete tests; use --testall for a full suite'
+    not pytest.config.getoption('--testall'),
+    reason='Ignoring slow or incomplete tests; use --testall for a full suite'
 )
 
+
 def f():
-	return 3
+    """Simple return for testing."""
+    return 3
+
 
 def test_number():
-	assert f() == 3
+    """Basic True test."""
+    assert f() == 3
+
 
 @slow
 def test_function():
-	assert f() == 4
+    """Parameter False test."""
+    assert f() == 4
