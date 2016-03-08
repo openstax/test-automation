eec69963f94407dbcb967c8607d8cd4792441287
Greg Fitch <greg.fitch@rice.edu>
2016-03-08 16:47:27 -0600

    add case id to test name

--- tutor/test_tutor_s2.py
+++ tutor/test_tutor_s2.py
@@ -8,34 +8,34 @@ TESTS = os.environ['CASELIST']
 
 # Case C7791 - S2 Case 1
 @pytest.mark.skipif(str(7791) not in TESTS, reason='Excluded')
-def test_case_1():
+def test_case_1_7791():
     """Parameter False test."""
     assert 1 != 4
 
 
 # Case C7792 - S2 Case 2
 @pytest.mark.skipif(str(7792) not in TESTS, reason='Excluded')
-def test_case_2():
+def test_case_2_7792():
     """Parameter False test."""
     assert 2 <= 4
 
 
 # Case C7793 - S2 Case 3
 @pytest.mark.skipif(str(7793) not in TESTS, reason='Excluded')
-def test_case_3():
+def test_case_3_7793():
     """Parameter False test."""
     assert 3 < 4
 
 
 # Case C7794 - S2 Case 4
 @pytest.mark.skipif(str(7794) not in TESTS, reason='Excluded')
-def test_case_4():
+def test_case_4_7794():
     """Parameter False test."""
     assert 4 == 4
 
 
 # Case C7795 - S2 Case 5
 @pytest.mark.skipif(str(7795) not in TESTS, reason='Excluded')
-def test_case_5():
+def test_case_5_7795():
     """Parameter False test."""
     assert 5 > 4
--- tutor/test_tutor_s3.py
+++ tutor/test_tutor_s3.py
@@ -8,13 +8,13 @@ TESTS = os.environ['CASELIST']
 
 # Case C7796 - S3 Case 1
 @pytest.mark.skipif(str(7796) not in TESTS, reason='Excluded')
-def test_case_1():
+def test_case_1_7796():
     """Parameter False test."""
     assert '1' != '4'
 
 
 # Case C7797 - S3 Case 2
 @pytest.mark.skipif(str(7797) not in TESTS, reason='Excluded')
-def test_case_2():
+def test_case_2_7797():
     """Parameter False test."""
     assert 2.0 == 4.0
