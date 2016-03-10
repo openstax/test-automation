c48363142be6c06ec5e7aa63aa32a294771cef7e
Amanda Shih <amanda.shih@rice.edu>
2016-03-10 04:35:38 -0600

    test multiple browsers at once

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var spawn = require('child_process').spawn;
+var _ = require('lodash');
 
 require('dotenv-safe').load();
-spawn('mocha', ['-R', 'spec', 'tutor/index.js'], {stdio: "inherit"});
+
+var browsers = ['chrome:36:Windows', 'firefox:36:Windows', 'internet explorer:10:Windows 7'];
+
+var mochas = _.map(browsers, function(browser){
+  var browserOptions = _.extend({}, process.env, {SELENIUM_BROWSER: browser});
+  return spawn('mocha', ['-R', 'spec', 'tutor/index.js'], {stdio: "inherit", env: browserOptions});
+});
