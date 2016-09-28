74231712f21eeb6c2f648a75e43b37a41e2785fa
sisiz <zimmerman.sisi@gmail.com>
2016-09-28 15:38:06 -0500

    1/2 done

--- tutor/test_t2_12_CreateNewQuestionAndAssignmentTypes.py
+++ tutor/test_t2_12_CreateNewQuestionAndAssignmentTypes.py
@@ -12,6 +12,7 @@ from random import randint
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as expect
 # from staxing.assignment import Assignment
+from selenium.common.exceptions import NoSuchElementException
 
 # select user types: Admin, ContentQA, Teacher, and/or Student
 from staxing.helper import Teacher, Student
@@ -25,12 +26,12 @@ basic_test_env = json.dumps([{
 BROWSERS = json.loads(os.getenv('BROWSERS', basic_test_env))
 TESTS = os.getenv(
     'CASELIST',
-    # str([
-    #     14739, 14741, 14742, 14743, 14744
-    # ])
-    str([14743])
+    str([
+        14739, 14741, 14742, 14743, 14744
+    ])
 )
-
+# 14743, 14744 - rely on add hw helpwer which has bugs
+# 14742 - tessting out of date feature?
 
 @PastaDecorator.on_platforms(BROWSERS)
 class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
@@ -43,14 +44,14 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
         self.desired_capabilities['name'] = self.id()
         self.teacher = Teacher(
             use_env_vars=True,
-            # pasta_user=self.ps,
-            # capabilities=self.desired_capabilities
+            pasta_user=self.ps,
+            capabilities=self.desired_capabilities
         )
         self.student = Student(
             use_env_vars=True,
             existing_driver=self.teacher.driver,
-            # pasta_user=self.ps,
-            # capabilities=self.desired_capabilities
+            pasta_user=self.ps,
+            capabilities=self.desired_capabilities
         )
 
     def tearDown(self):
@@ -91,25 +92,24 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
         self.ps.test_updates['passed'] = False
 
         # Test steps and verification assertions
-        self.teacher.driver.get("https://exercises-qa.openstax.org/")
-        self.teacher.page.wait_for_page_load()
+        self.teacher.get("https://exercises-qa.openstax.org/")
         # login
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//div[@id="account-bar-content"]//a[text()="Sign in"]'
         ).click()
         self.teacher.page.wait_for_page_load()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.ID, 'auth_key'
         ).send_keys(self.teacher.username)
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.ID, 'password'
         ).send_keys(self.teacher.password)
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//button[text()="Sign in"]'
         ).click()
         # create new vocab
         self.teacher.page.wait_for_page_load()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//a[@href="/exercises/new"]'
         ).click()
         self.teacher.wait.until(
@@ -141,25 +141,24 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
         self.ps.test_updates['passed'] = False
 
         # Test steps and verification assertions
-        self.teacher.driver.get("https://exercises-qa.openstax.org/")
-        self.teacher.page.wait_for_page_load()
+        self.teacher.get("https://exercises-qa.openstax.org/")
         # login
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//div[@id="account-bar-content"]//a[text()="Sign in"]'
         ).click()
         self.teacher.page.wait_for_page_load()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.ID, 'auth_key'
         ).send_keys(self.teacher.username)
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.ID, 'password'
         ).send_keys(self.teacher.password)
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//button[text()="Sign in"]'
         ).click()
         # create new vocab
         self.teacher.page.wait_for_page_load()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//a[@href="/exercises/new"]'
         ).click()
         self.teacher.wait.until(
@@ -167,7 +166,7 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
                 (By.XPATH, '//input[@label="True/False"]')
             )
         ).click()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH, '//span[text()="True/False"]'
         )
         self.ps.test_updates['passed'] = True
@@ -204,7 +203,7 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
 
     # NOT DONE
     # create hw helper still not working
-    # can test later half with manually created assignemnt
+    # but works for manually created assignemnt, add assignemnt commented out
     # 14743 - 004 - Teacher | Each part of a multi-part question counts as a
     # seperate problem when scored
     @pytest.mark.skipif(str(14743) not in TESTS, reason='Excluded')
@@ -232,7 +231,7 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
         # create a hw with a multi part question, and gice it a randomized name
         # ID: 12061@6 is multi part
         self.teacher.login()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH,
             '//div[@data-appearance="intro_sociology"]' +
             '//a[not(contains(@href,"/cc-dashboard"))]'
@@ -252,32 +251,33 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
             }
         )
         self.teacher.open_user_menu()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.LINK_TEXT, 'Student Scores'
         ).click()
         self.teacher.page.wait_for_page_load()
         # can just click the first review because assignemnt just created
         # and should be the most recent one
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.LINK_TEXT, 'Review'
         ).click()
-        cards = self.teacher.driver.find_elements(
+        cards = self.teacher.find_all(
             By.XPATH, '//div[contains(@class,"card-body")]')
-        breadcrumbs = self.teacher.driver.find_elements(
+        questions = self.teacher.find_all(
             By.XPATH, '//div[contains(@class,"openstax-question")]')
-        questions = self.teacher.driver.find_elements(
+        breadcrumbs = self.teacher.find_all(
             By.XPATH, '//span[contains(@class,"openstax-breadcrumb")]')
-        # check that more breadcrumbs than cards?
         assert(len(questions) == len(breadcrumbs)), \
             'breadcrumbs and questions not equal'
-        assert(len(cards) > len(breadcrumbs)), \
-            'multipart q card has multiple questions,' + \
+        assert(len(cards) < len(breadcrumbs)), \
+            'multipart question card has multiple questions,' + \
             'not matching up with  breadcrumbs'
 
         self.ps.test_updates['passed'] = True
 
     # NOT DONE
     # same issue as above w/ add_homework helper
+    # but works for manually created assignemnt
+    # (add assignemnt gets commented out, manual assignemnt name added)
     # 14744 - 005 - Student | Each part of a multi-part question counts as a
     # seperate problem when scored
     @pytest.mark.skipif(str(14744) not in TESTS, reason='Excluded')
@@ -303,11 +303,10 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
         self.ps.test_updates['passed'] = False
 
         # Test steps and verification assertions
-        raise NotImplementedError(inspect.currentframe().f_code.co_name)
-        # create a hw with a multi part question, and gice it a randomized name
+        # create a hw with a multi part question, and give it a randomized name
         # ID: 12252@5 is multi part
         self.teacher.login()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH,
             '//div[@data-appearance="intro_sociology"]' +
             '//a[not(contains(@href,"/cc-dashboard"))]'
@@ -327,13 +326,50 @@ class TestCreateNewQuestionAndAssignmentTypes(unittest.TestCase):
             }
         )
         self.teacher.logout()
+        # assignemnt name put here for manual testing
+        # assignment_name = 'hw w/ video and multi part question'
         # login as student and go to same class
         self.student.login()
-        self.teacher.driver.find_element(
+        self.teacher.find(
             By.XPATH,
             '//div[@data-appearance="intro_sociology"]' +
             '//a[not(contains(@href,"/cc-dashboard"))]'
         ).click()
+        self.student.page.wait_for_page_load()
         # go to assignment (find my assignemnt_name)
+        self.student.find(
+            By.XPATH,
+            '//a[contains(@class,"homework workable")]' +
+            '//span[text()="' + assignment_name + '"]'
+        ).click()
+        # go through all questions
+        breadcrumbs = self.teacher.find_all(
+            By.XPATH,
+            '//span[contains(@class,"openstax-breadcrumb")' +
+            ' and not(contains(@class,"intro"))' +
+            ' and not(contains(@class,"personalized"))'
+            ' and not(contains(@class,"end"))]')
+        total_questions = 0
+        found_multipart = False
+        i = 0
+        while i < len(breadcrumbs):
+            breadcrumbs[i].click()
+            try:
+                self.student.find(
+                    By.XPATH, '//span[text()="Multi-part question"]')
+                found_multipart = True
+                questions = self.teacher.find_all(
+                    By.XPATH, '//div[contains(@class,"openstax-question")]')
+                total_questions += len(questions)
+                i += len(questions)
+            except NoSuchElementException:
+                i += 1
+                questions = self.teacher.find_all(
+                    By.XPATH, '//div[contains(@class,"openstax-question")]')
+                total_questions += len(questions)
+        # check that everything worked out
+        assert(found_multipart), 'no multipart question found'
+        assert(total_questions == len(breadcrumbs)), \
+            'breadcrumbs and questions not equal'
 
         self.ps.test_updates['passed'] = True
