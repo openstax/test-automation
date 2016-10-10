b450c6958c17ca7ac5f0c9c172521fb4664ff8de
sisiz <zimmerman.sisi@gmail.com>
2016-10-10 10:48:42 -0500

    uncommented stuff for saucelabs

--- tutor/test_t2_10_ImproveAssignmentManagement.py
+++ tutor/test_t2_10_ImproveAssignmentManagement.py
@@ -50,16 +50,16 @@ class TestImproveAssignmentManagement(unittest.TestCase):
         self.desired_capabilities['name'] = self.id()
         self.teacher = Teacher(
             use_env_vars=True,
-            # pasta_user=self.ps,
-            # capabilities=self.desired_capabilities
+            pasta_user=self.ps,
+            capabilities=self.desired_capabilities
         )
         self.student = Student(
             username=os.getenv('STUDENT_USER'),
             password=os.getenv('STUDENT_PASSWORD'),
             site='https://tutor-qa.openstax.org',
             existing_driver=self.teacher.driver,
-            # pasta_user=self.ps,
-            # capabilities=self.desired_capabilities
+            pasta_user=self.ps,
+            capabilities=self.desired_capabilities
         )
 
     def tearDown(self):
