89355dfbec00c2968c3e67014735419b34225349
Greg Fitch <greg.fitch@rice.edu>
2016-11-21 15:53:08 -0600

    re-enable SL

--- tutor/test_t1_13_ViewTheCalendarDashboard.py
+++ tutor/test_t1_13_ViewTheCalendarDashboard.py
@@ -52,8 +52,8 @@ class TestViewTheCalendarDashboard(unittest.TestCase):
         self.desired_capabilities['name'] = self.id()
         self.teacher = Teacher(
             use_env_vars=True,
-            # pasta_user=self.ps,
-            # capabilities=self.desired_capabilities
+            pasta_user=self.ps,
+            capabilities=self.desired_capabilities
         )
         self.teacher.login()
         self.teacher.select_course(appearance='hs_physics')
