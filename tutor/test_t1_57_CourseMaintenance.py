71f2a622de23244edf2fd4206ca885b04dd634a6
nls5 <openstaxii@OpenStax.local>
2017-03-08 17:01:55 -0600

    Implemented new test cases for t1_57

--- tutor/test_t1_57_CourseMaintenance.py
+++ tutor/test_t1_57_CourseMaintenance.py
@@ -14,6 +14,8 @@ from selenium.webdriver.support import expected_conditions as expect
 
 # select user types: Admin, ContentQA, Teacher, and/or Student
 from staxing.helper import Admin
+from staxing.helper import Student
+from staxing.helper import Teacher
 
 
 basic_test_env = json.dumps([{
@@ -27,7 +29,9 @@ LOCAL_RUN = os.getenv('LOCALRUN', 'false').lower() == 'true'
 TESTS = os.getenv(
     'CASELIST',
     str([
-        8311, 8312, 8313, 8314, 8315
+        8311, 8312, 8313, 8314, 8315,
+        112519, 112520, 112521, 112522,
+        112523
     ])
 )
 
@@ -243,3 +247,339 @@ class TestCourseMaintenance(unittest.TestCase):
 
         if deleted:
             self.ps.test_updates['passed'] = True
+
+    # Case C112519 - 006 - Admin | Add an instructor-only system notification
+    @pytest.mark.skipif(str(112519) not in TESTS, reason='Excluded')
+    def test_admin_add_instructor_notification_112519(self):
+        """Add an instructor-only system notification.
+
+        Steps:
+
+        Expected Result:
+
+        """
+        self.ps.test_updates['name'] = 't1.57.006' \
+            + inspect.currentframe().f_code.co_name[4:]
+        self.ps.test_updates['tags'] = ['t1', 't1.57', 't1.57.006', '112519']
+        self.ps.test_updates['passed'] = False
+
+        # Test steps and verification assertions
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'System Setting')
+            )
+        ).click()
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'Notifications')
+            )
+        ).click()
+        self.admin.sleep(5)
+
+        self.admin.find(By.XPATH, "//div[3]//input[@id='message']").send_keys(
+            'automated test')
+
+        self.admin.driver.find_elements_by_xpath(
+            "//input[@class='btn btn-default']")[1].click()
+
+        self.admin.sleep(5)
+
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        for index, n in enumerate(notif):
+            if n.text.find('automated test') >= 0:
+                self.admin.driver.find_elements_by_xpath(
+                    "//a[@class='btn btn-warning']")[index].click()
+                self.admin.driver.switch_to_alert().accept()
+                self.ps.test_updates['passed'] = True
+                break
+
+        self.admin.sleep(5)
+
+        self.ps.test_updates['passed'] = True
+
+    # Case C112520 - 007 - Admin | Delete a instructor-only system notification
+    @pytest.mark.skipif(str(112520) not in TESTS, reason='Excluded')
+    def test_admin_delete_instructor_notification_112520(self):
+        """Delete an instructor-only system notification.
+
+        Steps:
+
+        Expected Result:
+
+        """
+        self.ps.test_updates['name'] = 't1.57.007' \
+            + inspect.currentframe().f_code.co_name[4:]
+        self.ps.test_updates['tags'] = ['t1', 't1.57', 't1.57.007', '112520']
+        self.ps.test_updates['passed'] = False
+
+        # Test steps and verification assertions
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'System Setting')
+            )
+        ).click()
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'Notifications')
+            )
+        ).click()
+        self.admin.sleep(5)
+
+        self.admin.find(By.XPATH, "//div[3]//input[@id='message']").send_keys(
+            'automated test')
+
+        self.admin.driver.find_elements_by_xpath(
+            "//input[@class='btn btn-default']")[1].click()
+
+        self.admin.sleep(5)
+
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        for index, n in enumerate(notif):
+            if n.text.find('automated test') >= 0:
+                self.admin.driver.find_elements_by_xpath(
+                    "//a[@class='btn btn-warning']")[index].click()
+                self.admin.driver.switch_to_alert().accept()
+                self.ps.test_updates['passed'] = True
+                break
+
+        self.admin.sleep(5)
+
+        deleted = True
+
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        for n in notif:
+            if n.text.find('automated test') >= 0:
+                deleted = False
+                break
+
+        assert(deleted), 'notification not deleted'
+
+        self.ps.test_updates['passed'] = True
+
+    # Case C112521 - 008 - Student | View an active system notification
+    @pytest.mark.skipif(str(112521) not in TESTS, reason='Excluded')
+    def test_student_view_active_notification_112521(self):
+        """View an active system notification.
+
+        Steps:
+
+        Expected Result: //div[@class='notification system']/span
+
+        """
+        self.ps.test_updates['name'] = 't1.57.008' \
+            + inspect.currentframe().f_code.co_name[4:]
+        self.ps.test_updates['tags'] = ['t1', 't1.57', 't1.57.008', '112521']
+        self.ps.test_updates['passed'] = False
+
+        # Test steps and verification assertions
+
+        # Create notification
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'System Setting')
+            )
+        ).click()
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'Notifications')
+            )
+        ).click()
+        self.admin.sleep(5)
+
+        self.admin.find(By.XPATH, "//input[@id='message']").send_keys(
+            'automated test')
+
+        self.admin.driver.find_elements_by_xpath(
+            "//input[@class='btn btn-default']")[0].click()
+
+        self.admin.sleep(5)
+
+        # View notification as student
+        student = Student(use_env_vars=True)
+        student.login()
+        student.select_course(appearance='physics')
+        student.sleep(10)
+        notifs = student.driver.find_elements_by_xpath(
+            "//div[@class='notification system']/span")
+
+        found = False
+
+        for notif in notifs:
+            if notif.text.find("automated test") >= 0:
+                found = True
+
+        student.delete()
+
+        # Delete notification
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        for index, n in enumerate(notif):
+            if n.text.find('automated test') >= 0:
+                self.admin.driver.find_elements_by_xpath(
+                    "//a[@class='btn btn-warning']")[index].click()
+                self.admin.driver.switch_to_alert().accept()
+                self.ps.test_updates['passed'] = True
+                break
+
+        self.admin.sleep(5)
+
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        assert(found), 'notification not seen'
+
+        self.ps.test_updates['passed'] = True
+
+    # Case C112522 - 009 - Teacher | View an active system notification
+    @pytest.mark.skipif(str(112522) not in TESTS, reason='Excluded')
+    def test_teacher_view_active_notification_112522(self):
+        """View an active system notification.
+
+        Steps:
+
+        Expected Result:
+
+        """
+        self.ps.test_updates['name'] = 't1.57.009' \
+            + inspect.currentframe().f_code.co_name[4:]
+        self.ps.test_updates['tags'] = ['t1', 't1.57', 't1.57.009', '112522']
+        self.ps.test_updates['passed'] = False
+
+        # Test steps and verification assertions
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'System Setting')
+            )
+        ).click()
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'Notifications')
+            )
+        ).click()
+        self.admin.sleep(5)
+
+        self.admin.find(By.XPATH, "//input[@id='message']").send_keys(
+            'automated test')
+
+        self.admin.driver.find_elements_by_xpath(
+            "//input[@class='btn btn-default']")[0].click()
+
+        self.admin.sleep(5)
+
+        # View notification as student
+        teacher = Teacher(use_env_vars=True)
+        teacher.login()
+        teacher.select_course(appearance='physics')
+        teacher.sleep(10)
+        notifs = teacher.driver.find_elements_by_xpath(
+            "//div[@class='notification system']/span")
+
+        found = False
+
+        for notif in notifs:
+            if notif.text.find("automated test") >= 0:
+                found = True
+
+        teacher.delete()
+
+        # Delete notification
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        for index, n in enumerate(notif):
+            if n.text.find('automated test') >= 0:
+                self.admin.driver.find_elements_by_xpath(
+                    "//a[@class='btn btn-warning']")[index].click()
+                self.admin.driver.switch_to_alert().accept()
+                self.ps.test_updates['passed'] = True
+                break
+
+        self.admin.sleep(5)
+
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        assert(found), 'notification not seen'
+
+        self.ps.test_updates['passed'] = True
+
+    # Case C112523 - 010 - Teacher | View an active instructor-only
+    # system notification
+    @pytest.mark.skipif(str(112523) not in TESTS, reason='Excluded')
+    def test_teacher_view_instructor_notification_112523(self):
+        """View an active instructor-only system notification.
+
+        Steps:
+
+        Expected Result:
+
+        """
+        self.ps.test_updates['name'] = 't1.57.010' \
+            + inspect.currentframe().f_code.co_name[4:]
+        self.ps.test_updates['tags'] = ['t1', 't1.57', 't1.57.010', '112523']
+        self.ps.test_updates['passed'] = False
+
+        # Test steps and verification assertions
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'System Setting')
+            )
+        ).click()
+        self.admin.wait.until(
+            expect.visibility_of_element_located(
+                (By.PARTIAL_LINK_TEXT, 'Notifications')
+            )
+        ).click()
+        self.admin.sleep(5)
+
+        self.admin.find(By.XPATH, "//div[3]//input[@id='message']").send_keys(
+            'automated test')
+
+        self.admin.driver.find_elements_by_xpath(
+            "//input[@class='btn btn-default']")[1].click()
+
+        self.admin.sleep(5)
+
+        # View notification as student
+        teacher = Teacher(use_env_vars=True)
+        teacher.login()
+        teacher.select_course(appearance='physics')
+        teacher.sleep(10)
+        notifs = teacher.driver.find_elements_by_xpath(
+            "//div[@class='notification system']/span")
+
+        found = False
+
+        for notif in notifs:
+            if notif.text.find("automated test") >= 0:
+                found = True
+
+        teacher.delete()
+
+        # Delete notification
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        for index, n in enumerate(notif):
+            if n.text.find('automated test') >= 0:
+                self.admin.driver.find_elements_by_xpath(
+                    "//a[@class='btn btn-warning']")[index].click()
+                self.admin.driver.switch_to_alert().accept()
+                self.ps.test_updates['passed'] = True
+                break
+
+        self.admin.sleep(5)
+
+        notif = self.admin.driver.find_elements_by_xpath(
+            "//div[@class='col-xs-12']")
+
+        assert(found), 'notification not seen'
+
+        self.ps.test_updates['passed'] = True
