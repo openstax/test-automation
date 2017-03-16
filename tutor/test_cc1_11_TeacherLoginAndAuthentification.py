73c58e81fea71a6caf8c2d0545c84ba593760765
sisiz <zimmerman.sisi@gmail.com>
2017-03-16 11:47:44 -0500

    all cases running locally

--- tutor/test_cc1_11_TeacherLoginAndAuthentification.py
+++ tutor/test_cc1_11_TeacherLoginAndAuthentification.py
@@ -9,7 +9,7 @@ import unittest
 from pastasauce import PastaSauce, PastaDecorator
 # from random import randint
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support import expected_conditions as expect
+from selenium.webdriver.support import expected_conditions as expect
 # from staxing.assignment import Assignment
 
 # select user types: Admin, ContentQA, Teacher, and/or Student
@@ -49,7 +49,6 @@ class TestTeacherLoginAndAuthentification(unittest.TestCase):
             self.teacher = Teacher(
                 use_env_vars=True
             )
-        self.teacher.login()
 
     def tearDown(self):
         """Test destructor."""
@@ -80,27 +79,77 @@ class TestTeacherLoginAndAuthentification(unittest.TestCase):
         """
         self.ps.test_updates['name'] = 'cc1.11.001' \
             + inspect.currentframe().f_code.co_name[4:]
-        self.ps.test_updates['tags'] = [
-            'cc1',
-            'cc1.11',
-            'cc1.11.001',
-            '7688'
-        ]
+        self.ps.test_updates['tags'] = ['cc1', 'cc1.11', 'cc1.11.001', '7688']
         self.ps.test_updates['passed'] = False
 
         # Test steps and verification assertions
-        self.teacher.select_course(appearance='macro_economics')
-        self.teacher.sleep(5)
+        self.teacher.get(self.teacher.url)
+        self.teacher.page.wait_for_page_load()
+        # check to see if the screen width is normal or condensed
+        if self.teacher.driver.get_window_size()['width'] <= \
+           self.teacher.CONDENSED_WIDTH:
+            # get small-window menu toggle
+            is_collapsed = self.teacher.find(
+                By.XPATH,
+                '//button[contains(@class,"navbar-toggle")]'
+            )
+            # check if the menu is collapsed and, if yes, open it
+            if('collapsed' in is_collapsed.get_attribute('class')):
+                is_collapsed.click()
+        self.teacher.wait.until(
+            expect.visibility_of_element_located(
+                (By.LINK_TEXT, 'Log in')
+            )
+        ).click()
+        self.teacher.page.wait_for_page_load()
+        self.teacher.find(
+            By.ID, 'login_username_or_email'
+        ).send_keys(self.teacher.username)
+        self.teacher.find(By.CSS_SELECTOR, '.primary').click()
+        self.teacher.find(
+            By.ID, 'login_password'
+        ).send_keys(self.teacher.password)
+        self.teacher.find(By.CSS_SELECTOR, '.primary').click()
+        self.teacher.page.wait_for_page_load()
+        # check if a password change is required
+        if 'reset your password' in self.teacher.driver.page_source.lower():
+            try:
+                self.teacher.find(By.ID, 'set_password_password') \
+                    .send_keys(self.teacher.password)
+                self.teacher.find(
+                    By.ID, 'set_password_password_confirmation') \
+                    .send_keys(self.teacher.password)
+                self.teacher.find(By.CSS_SELECTOR, '.primary').click()
+                self.teacher.sleep(1)
+                self.teacher.find(By.CSS_SELECTOR, '.primary').click()
+            except Exception as e:
+                raise e
+        self.teacher.page.wait_for_page_load()
+        source = self.teacher.driver.page_source.lower()
+        print('Reached Terms/Privacy')
+        while 'terms of use' in source or 'privacy policy' in source:
+            self.teacher.accept_contract()
+            self.teacher.page.wait_for_page_load()
+            source = self.teacher.driver.page_source.lower()
+        assert('dashboard' in self.teacher.current_url()),\
+            'Not taken to dashboard: %s' % self.teacher.current_url()
 
-        assert('cc-dashboard' in self.teacher.current_url()), \
-            'Not viewing the cc dashboard'
+        self.teacher.driver.find_element(
+            By.XPATH,
+            '//p[contains(text(),"OpenStax Concept Coach")]'
+        ).click()
+        self.teacher.driver.find_element(
+            By.XPATH,
+            '//span[text()="Class Dashboard"]'
+        )
 
         self.ps.test_updates['passed'] = True
 
-    # Case C7689 - 002 - Teacher | Logging out returns to the login page
+    # Case C7689 - 002 - Teacher | Logging out returns to the Concept Coach
+    # landing page
     @pytest.mark.skipif(str(7689) not in TESTS, reason='Excluded')
-    def test_teacher_loggin_out_returns_to_the_login_page_7689(self):
-        """Logging out returns to the login page.
+    def test_teacher_loggin_out_returns_to_concept_coach_landing_pa_7689(self):
+        """Logging out returns to the Concept Coach landing page.
 
         Steps:
         Click the user menu containing the user's name
@@ -111,24 +160,18 @@ class TestTeacherLoginAndAuthentification(unittest.TestCase):
         """
         self.ps.test_updates['name'] = 'cc1.11.002' \
             + inspect.currentframe().f_code.co_name[4:]
-        self.ps.test_updates['tags'] = [
-            'cc1',
-            'cc1.11',
-            'cc1.11.002',
-            '7689'
-        ]
+        self.ps.test_updates['tags'] = ['cc1', 'cc1.11', 'cc1.11.002', '7689']
         self.ps.test_updates['passed'] = False
 
         # Test steps and verification assertions
-        self.teacher.select_course(appearance='macro_economics')
-        self.teacher.sleep(5)
-
-        assert('dashboard' in self.teacher.current_url()), \
-            'Not viewing the cc dashboard'
-
+        self.teacher.login()
+        self.teacher.driver.find_element(
+            By.XPATH,
+            '//p[contains(text(),"OpenStax Concept Coach")]'
+        ).click()
         self.teacher.open_user_menu()
         self.teacher.sleep(1)
-        self.teacher.find(By.XPATH, "//a/form[@class='-logout-form']").click()
+        self.teacher.find(By.XPATH, "//a//input[@value='Log out']").click()
 
         assert('cc.openstax.org' in self.teacher.current_url()), \
             'Not viewing the calendar dashboard'
